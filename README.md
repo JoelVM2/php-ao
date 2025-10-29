b4472fbce990219a1a6edbbe7a41203a07bc8412
Joel <Joel.vargasmedal@gmail.com>
2025-10-29 20:37:11 +0100

    Added crud functionalities, this time for real

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 Actividad obligatoia de DWES
+
+En powershell
+
+php .\TaskManager.php list,  php .\TaskManager.php list 1 o php .\TaskManager.php list --completed
+
+php .\TaskManager.php edit 2 "Tarea" "Comprar Todo" "2025-10-28", tienes que poner todos los parametros para editar una tarea
+
+php .\TaskManager.php add "Tareita" "No comprar nada" "2025-10-14" Añadir una tarea
+
+php .\TaskManager.php delete 4 o php .\TaskManager.php delete 4 all Borrar tareas
+
+php .\TaskManager.php complete 3 completar tarea por id
--- TaskManager.php
+++ TaskManager.php
@@ -7,10 +7,8 @@ $fp       = fopen($filename, "r+");
 initialize();
 $argv[0];
 
-
-
-
-function initialize(){
+function initialize()
+{
     global $fp;
     global $filesize;
     global $fp;
@@ -29,23 +27,23 @@ function initialize(){
         case 'list';
             listTask();
             break;
+        case 'complete';
+            taskCompleted();
+            break;
         default:
             // Devolver mensaje con los parametros posibles
             break;
     }
-
 }
 
-// hacer que funcione esta bazofia con tasks, y que no modifique esa parte del json
-
-function addTask(){
+function addTask()
+{
     global $fp;
     global $filesize;
     global $fp;
     global $argv;
 
-    if (flock($fp, LOCK_EX))
-    {   
+    if (flock($fp, LOCK_EX)) {
         $data = json_decode(fread($fp, $filesize), true);
         $last_item    = end($data);
         $last_item_id = $last_item['id'];
@@ -57,101 +55,151 @@ function addTask(){
             'dueDate' => $argv[4],
             'completed' => false
         );
+        echo "Task created.";
+        $data = array_values($data);
+        fseek($fp, 0);
+        ftruncate($fp, 0);
+        fwrite($fp, json_encode($data, JSON_PRETTY_PRINT));
+        flock($fp, LOCK_UN);
+    } else {
+        echo "Unable to lock file";
+    }
+    fclose($fp);
+    
+}
+
+function deleteTask()
+{
+    global $fp;
+    global $filesize;
+    global $fp;
+    global $argv;
+
+    if (flock($fp, LOCK_EX)) {
+        $data = json_decode(fread($fp, $filesize), true);
+
+        $count = 0;
+       
+        foreach ($data as $item) {
+            if ($item['id'] == $argv[2]) {
+                unset($data[$count]);
+                echo 'Task deleted.';
+            }
+            if($argv[2]=='all'){
+                unset($data[$count]);
+                echo 'All tasks deleted.';
+            }
+            $count++;
+        }
 
         $data = array_values($data);
         fseek($fp, 0);
         ftruncate($fp, 0);
         fwrite($fp, json_encode($data, JSON_PRETTY_PRINT));
         flock($fp, LOCK_UN);
-    }else{
-        echo "Unable to lock file";
-    }
-    fclose($fp);
-    echo "Has introducido la tarea" ;
-}
-
-function deleteTask(){
-    global $fp;
-    global $filesize;
-    global $fp;
-    global $argv;
-
-    
-    echo "Task deleted";
-
-    if (flock($fp, LOCK_EX))
-    {   
-        $data = json_decode(fread($fp, $filesize), true);
-    
-        unset($data[$argv[2]-1]);
-
-        $data = array_values($data);
-        fseek($fp, 0);
-        ftruncate($fp, 0);
-        fwrite($fp, json_encode($data, JSON_PRETTY_PRINT));
-        flock($fp, LOCK_UN);
-    }else{
+    } else {
         echo "Unable to lock file";
     }
     fclose($fp);
 }
 
-function editTask(){
+function editTask()
+{
     global $fp;
     global $filesize;
     global $fp;
     global $argv;
 
-    if (flock($fp, LOCK_EX))
-    {   
+    if (flock($fp, LOCK_EX)) {
         $data = json_decode(fread($fp, $filesize), true);
 
-        foreach($data['tasks'] as $item){
-            echo 'hola';
-            if($item['id'] == $argv[2]){
-                $item['id'] = $argv[2];
+        foreach ($data as &$item) {
+            if ($item['id'] == $argv[2]) {
+                $item['id'] = (int)$argv[2];
                 $item['title'] = $argv[3];
                 $item['description'] = $argv[4];
                 $item['dueDate'] = $argv[5];
                 $item['completed'] = false;
             }
         }
-        
+        fseek($fp, 0);
+        ftruncate($fp, 0);
+        fwrite($fp, json_encode($data, JSON_PRETTY_PRINT));
+        flock($fp, LOCK_UN);
+    } else {
+        echo "Unable to lock file";
+    }
+    fclose($fp);
+    echo "Task modified";
+}
+
+function listTask()
+{
+    global $fp;
+    global $filesize;
+    global $fp;
+    global $argv;
+    $data = json_decode(fread($fp, $filesize), true);
+
+    if($argv[2] == '--completed'){
+        echo 'hola';
+        foreach ($data as $item) {
+            if ($item['completed'] == true) {
+                listJson($item);
+            }
+        }
+        return;
+    }
+
+    if (!$argv[2] == null) {
+       
+        foreach ($data as $item) {
+            if ($item['id'] == $argv[2]) {
+                listJson($item);
+            }
+        }
+        return;
+    }
+ 
+    foreach ($data as $item) {
+        listJson($item);
+    }
+   
+}
+
+
+function listJson($item)
+{
+    $status = $item['completed'] ? 'true' : 'false';
+    echo  PHP_EOL . 'Task with Id: ' . $item['id'] . PHP_EOL .
+        'Title: ' . $item['title'] . PHP_EOL .
+        'Description: ' . $item['description'] . PHP_EOL .
+        'Date: ' . $item['dueDate'] . PHP_EOL .
+        'Status: ' . $status . PHP_EOL;
+}
+
+
+function taskCompleted(){
+    global $fp;
+    global $filesize;
+    global $fp;
+    global $argv;
+
+    if (flock($fp, LOCK_EX)) {
+        $data = json_decode(fread($fp, $filesize), true);
+        foreach ($data as &$item) {
+            if ($item['id'] == $argv[2]) {
+                $item['completed'] = true;
+            }
+        }
+        echo 'Task completed!';
         $data = array_values($data);
         fseek($fp, 0);
         ftruncate($fp, 0);
         fwrite($fp, json_encode($data, JSON_PRETTY_PRINT));
         flock($fp, LOCK_UN);
-    }else{
+    } else {
         echo "Unable to lock file";
     }
     fclose($fp);
-    echo "Has modificado la tarea" ;
 }
-
-function listTask(){
-     global $fp;
-    global $filesize;
-    global $fp;
-    global $argv;
-
-    if (flock($fp, LOCK_EX))
-    {   
-        $data = json_decode(fread($fp, $filesize), true);
-        foreach($data['tasks'] as $item){
-            $status = $item['completed'] ? 'true' : 'false';
-             echo  PHP_EOL.'Task with Id: ' . $item['id'] . PHP_EOL.
-                'Title: '.$item['title']. PHP_EOL.
-                'Description: '.$item['description'] . PHP_EOL.
-                'Date: '.$item['due_date']. PHP_EOL.
-                'Status: '.$status . PHP_EOL;
-        }
-    }else{
-        echo "Unable to lock file";
-    }
-    fclose($fp);
-    
-}
-
-
-?>
